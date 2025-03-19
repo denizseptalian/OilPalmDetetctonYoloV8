f7925a15de3350643a8558101a2eb1ced5ec5614
deniz <106682572+denizseptalian@users.noreply.github.com>
2025-03-19 11:19:22 +0700

    Update oilpalmindordetetction.py

--- oilpalmindordetetction.py
+++ oilpalmindordetetction.py
@@ -34,7 +34,7 @@ st.title("Deteksi dan Klasifikasi Kematangan Buah Sawit")
 st.image("Buah-Kelapa-Sawit.jpg", use_column_width=True)
 
 # Pilihan model
-model_option = st.selectbox("Pilih model deteksi:", ["Indoor (best.pt)", "Outdoor (best1.pt)"])
+model_option = st.selectbox("Pilih model deteksi:", ["Indoor : Untuk Deteksi Dataset Pada Mesin Grading (best.pt)", "Outdoor : Untuk Deteksi Pada Area Perkebunan (best1.pt)"])
 model_path = "best.pt" if model_option == "Indoor (best.pt)" else "best1.pt"
 
 uploaded_file = st.file_uploader("Unggah gambar", type=["jpg", "png", "jpeg"])
